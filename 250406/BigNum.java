a49affece5eb92a46f68ae6d60beee56f2b54c54
opeach2v <opeach2v@naver.com>
2025-04-06 23:08:30 +0900

    gg

--- 250406/BigNum.java
+++ 250406/BigNum.java
@@ -1,22 +1,30 @@
 public class BigNum {
     public String solution(String number, int k) {
         String answer = "";
-        String max = "";
+        String temp = "";
         String[] numS = number.split("");
         String[] maxStrings = new String[number.length() - k]; // 가장 큰 수들 골라서 저장 (전체 길이 중 k만큼 뺀 수만 존재해야 함.)
         
-        // 가장 큰 수 찾기
-        for(int i = 0; i < numS.length; i++) {
-            if(Integer.parseInt(numS[i]) < Integer.parseInt(numS[i])) {
+        int maxIndex = 0;
+        int i = 0;
+        // 맨 처음부터 큰 수들을 찾아서 저장장
+        while(i < numS.length) {
+            for(int a = 0; a < numS.length; a++) {
+                if(numS[maxIndex] < numS[a]) {
+                    
+                }
             }
         }
+    }
 
-        maxStrings[0] = max;    // 가장 큰 숫자는 미리 넣어두고
-        // 가장 큰 숫자를 기준으로 number의 길이에서 k만큼 뺀만큼의 큰 숫자 저장하기
-        for(int i = 0; i < numS.length - k; i++) {
+    public static void main(String[] args) {
+        String number = "1231234";
+        int k = 3;
+        String result = "";
 
-        }
+        BigNum b = new BigNum();
+        result = b.solution(number, k);
 
-        return answer;
+        System.out.println(result);
     }
 }
