d73277f5451270a99fdb88b9af24cadd7abf7168
opeach2v <opech2v@naver.com>
2025-04-07 18:29:31 +0900

    I'm died

--- 250405/BaboKeyborad.java
+++ 250405/BaboKeyborad.java
@@ -1,30 +1,39 @@
-import java.util.*;
-
 public class BaboKeyborad {
-    public static void main(String[] args) {
-        String[] keymap = {};
-        String[] targets = {};
-        int[] result = {};
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = {};
+        int keyIndex = 0;
+        int tarCount = 0;
+        int targetCount = 0;    // target은 처음부터 시작해서 카운트 나눠씀
 
-        Scanner sc = new Scanner(System.in);
-        
-        String[] keyS = {};    // keymap split
-        String[] tarS = {};    // target split
-
-        for(int a = 0; a < keymap.length; a++) {   // 타겟 계산이 끝날 때까지 반복
-            for(int b = 0; b < keymap[a].length(); b++) {
-                keyS = keymap[a].split("");
-            }
-            for(int c = 0; c < targets.length; c++) {   // 타겟 계산이 끝날 때까지 반복
-                for(int d = 0; d < targets[c].length(); d++) {
-                    tarS = targets[c].split("");
+        String[] keyS = {}; // keymap의 split
+        String[] tarS = {}; // targets의 split
+        tarS = targets[tarCount].split("");    // 타켓은 처음 문자열부터 먼저 끝내고 나서 그 뒤 계산함
+        while(true) {
+            while(keyIndex < keymap.length) {
+                for(int i = 0; i < tarS.length; i++) {
+                    if(!keymap[keyIndex].startsWith(tarS[i])) {    // target의 문자들이 keymap의 첫문자로 시작되는지 비교
+                        keyIndex++;
+                        continue;
+                    }
+                    else {
+                        targetCount++;
+                        continue;
+                    }
+    
                 }
             }
         }
 
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] keymap = {"ABACD", "BCEFD"};
+        String[] targets = {"ABCD","AABB"};
+        int[] result = {};
+
         for(int z = 0; z < result.length; z++) {
-            System.out.println(result);
+            System.out.print(result +" ");
         }
-        sc.close();
     }
 }
