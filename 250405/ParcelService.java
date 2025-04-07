c254be9f1fa51ca487dd9035dcd65ff1f3e0b97a
opeach2v <opech2v@naver.com>
2025-04-07 15:57:38 +0900

    ParcelService complate

--- 250405/ParcelService.java
+++ 250405/ParcelService.java
@@ -1,26 +1,19 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class ParcelService {
-    public static void main(String[] args) {
-        int n, w;   // n은 택배 번호, w는 가로 길이
-        int num;    // 꺼내려는 택배 상자
-        int line = 1;    // 현재 계산 중인 박스 줄
+    public int solution(int n, int w, int num) {    // 22, 6, 8
+        int line = 0;    // 현재 계산 중인 박스 줄
         int index = 0;  // w에서 몇 번째인지
         int up = 0; // 꺼내야 하는 택배 상자 윗 줄에 있을 상자
         int result = 1; // 꺼내려는 상자(미리 1 저장) + 꺼내기 위해 꺼내야 하는 상자
-
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        w = sc.nextInt();
-        num = sc.nextInt();
-
+        
         int[] haha = new int[w];
         int[] haha2 = new int[w];    // haha의 반대
 
-        // 1, 3, 5, 7, 9, 11, 13 등.. 저장
+        // 1, 3, 5, 7, 9, 11, 13 등.. haha에 저장
         int a = 1;
         for(int j = 0; j < w; j++) {
-            if(a % 2 == 1) {
+            if(a % 2 == 1) {    // 홀수들만 저장
                 haha[j] = a;
                 a++;
             }
@@ -36,45 +29,69 @@ public class ParcelService {
             a++;
         }
 
-        for(int i = 1; i <= n; i++) {   // 모든 상자 검색
+        for(int i = 1; i <= n; i++) {   // 모든 상자 순차적으로 확인
             if(i == num) {  // 꺼내야 하는 택배 상자 만나면
                 line = num / w; // 몇 줄?
-                if(num % w == 0) {
-                    line--;
+                if(num % w != 0) {  // 나머지가 0이 아니면 +1해줘야 함
+                    line++;
                 }
-                index = num % w;  // 몇번째 haha?
-                if(index == 0) {
-                    index++;
-                }
+                index = num % w;  // 몇번째 인덱스에 있는지
+                // index가 0이면 그대로 배열 적용하면 됨.
+                // But, 0다음엔 5, 4, 3, 2, 1이 됨. ex) 0 5 4 3 2 1 배열이라는 소리
                 if(line == 1 || line == 3 || line == 5 || line == 7 || line == 9) {    // 홀수 줄
-                    up = num + haha[index];
+                    // 따라서 홀수 줄이면서 index가 0이 아니면
+                    if(index != 0) {
+                        up = num + haha2[index - 1];
+                    }
+                    else {
+                        up = num + haha[0];
+                    }
                     if(up > n) {
                         break;
                     }
                     else {
                         result++;
                         num = up;
-                        line = 1;
+                        line = 0;
                         index = 0;
                     }
                 }
-                else {  // 짝수 줄이면
-                    up = num + haha2[index];
+                else {  // 짝수 줄이면서
+                    if(index != 0) {
+                        up = num + haha2[index - 1];
+                    }
+                    else {
+                        up = num + haha[0];
+                    }
                     if(up > n) {
                         break;
                     }
                     else {
                         result++;
                         num = up;
-                        line = 1;
+                        line = 0;
                         index = 0;
                     }
                 }
             }
         }
+        return result;
+    }
+    public static void main(String[] args) {
+        int n = 22, w = 6;   // n은 택배 번호, w는 가로 길이
+        int num = 8;    // 꺼내려는 택배 상자
+        int result = 0; // 꺼내려는 상자(미리 1 저장) + 꺼내기 위해 꺼내야 하는 상자
+
+        // Scanner sc = new Scanner(System.in);
+        // n = sc.nextInt();
+        // w = sc.nextInt();
+        // num = sc.nextInt();
+        
+        ParcelService sol = new ParcelService();
+        result = sol.solution(n, w, num);
 
         System.out.println(result);
 
-        sc.close();
+        // sc.close();
     }
 }
--- 250406/BigNum.java
+++ 250406/BigNum.java
@@ -1,25 +1,79 @@
 public class BigNum {
     public String solution(String number, int k) {
         String answer = "";
-        String temp = "";
+        int min = 0;
         String[] numS = number.split("");
-        String[] maxStrings = new String[number.length() - k]; // 가장 큰 수들 골라서 저장 (전체 길이 중 k만큼 뺀 수만 존재해야 함.)
-        
-        int maxIndex = 0;
+        int[] saveIndex = new int[number.length() - k];  // 인덱스 저장 후 이걸로 잠시 저장해서 비교할..
+
+        int count = 0;  // number.length - k 만큼 카운트 되면 끝나게 해야 함.
         int i = 0;
-        // 맨 처음부터 큰 수들을 찾아서 저장장
-        while(i < numS.length) {
-            for(int a = 0; a < numS.length; a++) {
-                if(numS[maxIndex] < numS[a]) {
-                    
+        int j = 0;
+        while(count < k) {
+            i = 0;
+            while(i <= number.length() - k) {  // k만큼 제거 했을 때 남을 것을 예상
+                for(j = 0; j < number.length(); j++) {
+                    if(i == 6) {
+                        break;
+                    }
+                    if(numS[saveIndex[i]] == "") {  // 비어있을 경우 한 번 건너뛰어서 저장
+                        for(int a = 0; a < numS.length; a++) {
+                            if(!numS[a].equals("")) {
+                                saveIndex[i] = saveIndex[i + 1];
+                                i++;
+                            }
+                            else {
+                                j += 2;
+                                break;
+                            }
+                        }
+                    }
+                    saveIndex[i] = j;
+                    i++;
+                }
+                if(i == 6) {
+                    i = 0;
+                    break;
+                }
+            }
+            for(i = 0; i < number.length() - k; i++) {
+                if(Integer.parseInt(numS[saveIndex[i]]) < Integer.parseInt(numS[saveIndex[i + 1]])) {   // 첫 숫자가 다음 숫자보다 작을 경우
+                    numS[saveIndex[i]] = numS[saveIndex[i]].replace(numS[saveIndex[i]], "");    // 그 숫자 없앰
+                    count++;
+                    break;
+                }
+                if(saveIndex[(number.length() - k) - 1] == number.length() - k) {   // 끝까지 왔을 때 카운트가 남아 있으면
+                    if(count < k) { // 가장 작은 숫자를 삭제
+                        min = saveIndex[0];
+                        for(j = 1; j < number.length() - k; j++) {
+                            if(Integer.parseInt(numS[min]) > Integer.parseInt(numS[saveIndex[j]])) {
+                                int temp = min;
+                                min = saveIndex[j];
+                                saveIndex[j] = temp;    // 인덱스 바꾸기 (최종적으로 작은 값이 들은 인덱스가 min에 들어올 것)
+                            }
+                        }
+                        // 최종적으로 가장 작은 값을 들은 인덱스의 값을 비우기
+                        numS[min] = numS[min].replace(numS[min], "");
+                        count++;
+                        if(count == k) {
+                            break;
+                        }
+                    }
                 }
             }
         }
+
+        for(i = 0; i < number.length(); i++) {
+            if(!numS[i].equals("")) {
+                answer += numS[i];
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
-        String number = "1231234";
-        int k = 3;
+        String number = "4177252841";
+        int k = 4;
         String result = "";
 
         BigNum b = new BigNum();
